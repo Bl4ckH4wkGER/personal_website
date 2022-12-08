59ee63b024785d287d294934c878edf378b302b8
Sven Otto <svenotto3110@gmail.com>
2022-12-08 15:58:23 -0800

    contact form components

--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,37 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+import EmailIcon from '@mui/icons-material/Email';
 
 function Contact() {
   return (
     <Container>
-      <Box>
-        <h2>Contact section</h2>
+      <h2>
+        <EmailIcon />
+        Get in touch.
+      </h2>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' }
+        }}
+        noValidate
+        autoComplete="off">
+        <div>
+          <TextField id="name" defaultValue="Name" />
+          <TextField id="email" defaultValue="Email" />
+          <TextField id="message" multiline rows={4} defaultValue="Message" />
+          <Button
+            variant="outlined"
+            onClick={() => {
+              alert('clicked');
+            }}>
+            Submit Message
+          </Button>
+        </div>
       </Box>
     </Container>
   );
