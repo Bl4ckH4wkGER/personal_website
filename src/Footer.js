1c3cf354ba29922c4f8e573989f48dbbdf8e5d23
Sven Otto <svenotto3110@gmail.com>
2022-12-18 14:32:13 -0800

    misc css updated in header, intro, footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,41 +2,42 @@ import * as React from 'react';
 import './Footer.css';
 
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
-import Fab from '@mui/material/Fab';
 
-import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function Footer() {
   return (
-    <Container maxWidth="sm">
-      <Fab color="primary" aria-label="up" href="#intro">
-        <ArrowCircleUpIcon />
-      </Fab>
-      <Box>
-        <div>
-          <Stack direction="row">
-            <a
-              aria-label="Github"
-              href="https://github.com/Bl4ckH4wkGER"
-              target="_blank"
-              rel="noreferrer noopener">
-              <GitHubIcon />
-            </a>
-            <a
-              aria-label="LinkedIn"
-              href="https://www.linkedin.com/in/sven-otto/"
-              target="_blank"
-              rel="noreferrer noopener">
-              <LinkedInIcon />
-            </a>
-          </Stack>
-        </div>
-      </Box>
-    </Container>
+    <Box>
+      <div className="footer">
+        <a className="upPageArrrow" href="#intro">
+          <KeyboardArrowUpIcon />
+        </a>
+        <Stack direction="row" justifyContent="center" alignItems="center">
+          <a
+            className="socialLinksFooter"
+            aria-label="Github"
+            href="https://github.com/Bl4ckH4wkGER"
+            target="_blank"
+            rel="noreferrer noopener">
+            <GitHubIcon />
+          </a>
+          <a
+            className="socialLinksFooter"
+            aria-label="LinkedIn"
+            href="https://www.linkedin.com/in/sven-otto/"
+            target="_blank"
+            rel="noreferrer noopener">
+            <LinkedInIcon />
+          </a>
+        </Stack>
+        <p>
+          <b>© Copyright 2022 Sven Otto</b>
+        </p>
+      </div>
+    </Box>
   );
 }
 export default Footer;
--- src/Intro.js
+++ src/Intro.js
@@ -4,17 +4,16 @@ import './Intro.css';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Fab from '@mui/material/Fab';
 
-import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function introSection() {
   return (
     <Container id="intro">
-      <Box sx={{ bgcolor: '#cfe8fc', height: '80vh' }}>
+      {/* <Box className="introText" sx={{ bgcolor: '#cfe8fc', height: '80vh' }}> */}
+      <Box className="introText">
         <h1>I&apos;m Sven Otto.</h1>
         <p>
           I&apos;m a greater-Seattle based Product Manager currently working at Geocaching HQ. My
@@ -28,6 +27,7 @@ function introSection() {
         <div>
           <Stack direction="row">
             <a
+              className="socialLinksIntro"
               aria-label="Github"
               href="https://github.com/Bl4ckH4wkGER"
               target="_blank"
@@ -35,6 +35,7 @@ function introSection() {
               <GitHubIcon />
             </a>
             <a
+              className="socialLinksIntro"
               aria-label="LinkedIn"
               href="https://www.linkedin.com/in/sven-otto/"
               target="_blank"
@@ -42,21 +43,19 @@ function introSection() {
               <LinkedInIcon />
             </a>
           </Stack>
-        </div>
-        <div>
           <Stack spacing={2} direction="row">
-            <Button variant="outlined">
-              <a href="#resume">Resume</a>
-            </Button>
-            <Button variant="outlined">
-              <a href="#contact">Contact</a>
-            </Button>
+            <a className="buttonLinks" href="#resume">
+              Resume
+            </a>
+            <a className="buttonLinks" href="#contact">
+              Contact
+            </a>
           </Stack>
+          <a className="downPageArrrow" href="#about">
+            <KeyboardArrowDownIcon />
+          </a>
         </div>
       </Box>
-      <Fab color="primary" aria-label="down" href="#about">
-        <ArrowCircleDownIcon />
-      </Fab>
     </Container>
   );
 }
