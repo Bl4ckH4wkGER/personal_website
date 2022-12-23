451efb651f328e82e1df79e41690fa43a35c219d
Sven Otto <svenotto3110@gmail.com>
2022-12-23 10:04:30 -0800

    contact section

--- src/Contact.js
+++ src/Contact.js
@@ -2,10 +2,8 @@ import * as React from 'react';
 import './Contact.css';
 
 import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
 
 import EmailIcon from '@mui/icons-material/Email';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
@@ -15,65 +13,51 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 function Contact() {
   return (
     <Container id="contact">
-      <h2>
-        <EmailIcon />
+      <h2 className="contactHeading">
+        <EmailIcon sx={{ width: '48px', height: '48px' }} />
         Get in touch.
       </h2>
-      <Box
-        component="form"
-        sx={{
-          '& .MuiTextField-root': { m: 1, width: '25ch' }
-        }}
-        noValidate
-        autoComplete="off">
-        <TextField id="name" defaultValue="Name" />
-        <TextField id="email" defaultValue="Email" />
-        <TextField id="message" multiline rows={4} defaultValue="Message" />
-        <Button
-          variant="outlined"
-          onClick={() => {
-            alert('clicked');
-          }}>
-          Submit Message
-        </Button>
-      </Box>
-      <Box>
-        <p>Thanks for stopping by!</p>
-        <p>
-          Did something spark your interest? Don&apos;t hesitate to reach out. I&apos;m always
-          availble to talk Product Management, Agile, or other shared interests.
-        </p>
-        <Stack direction="column">
-          <a className="contactLinks" href="mailto:svenotto3110@gmail.com">
-            <EmailIcon />
-            svenotto3110@gmail.com
-          </a>
-          <a
-            className="contactLinks"
-            href="https://www.google.com/maps/place/Poulsbo,+WA+98370/@47.7420916,-122.6788777,13z"
-            target={'_blank'}
-            rel="noreferrer">
-            <LocationOnIcon />
-            Poulsbo, WA
-          </a>
-          <a
-            className="contactLinks"
-            href="https://www.linkedin.com/in/sven-otto"
-            target={'_blank'}
-            rel="noreferrer">
-            <LinkedInIcon />
-            Sven Otto
-          </a>
-          <a
-            className="contactLinks"
-            href="https://github.com/Bl4ckH4wkGER"
-            target={'_blank'}
-            rel="noreferrer">
-            <GitHubIcon />
-            Bl4ckH4wkGER
-          </a>
-        </Stack>
-      </Box>
+      <Grid container className="contactBox" direction={{ xs: 'column', sm: 'column', md: 'row' }}>
+        <Grid item className="contactText" xs={6}>
+          <p>Thanks for stopping by!</p>
+          <p>
+            Did something spark your interest? Don&apos;t hesitate to reach out. I&apos;m always
+            availble to talk Product Management, Agile, or other shared interests.
+          </p>
+        </Grid>
+        <Grid item className="contactLinks" xs={6}>
+          <Stack direction="column">
+            <a className="contactLinks" href="mailto:svenotto3110@gmail.com">
+              <EmailIcon />
+              svenotto3110@gmail.com
+            </a>
+            <a
+              className="contactLinks"
+              href="https://www.google.com/maps/place/Poulsbo,+WA+98370"
+              target={'_blank'}
+              rel="noreferrer">
+              <LocationOnIcon />
+              Poulsbo, WA
+            </a>
+            <a
+              className="contactLinks"
+              href="https://www.linkedin.com/in/sven-otto"
+              target={'_blank'}
+              rel="noreferrer">
+              <LinkedInIcon />
+              Sven Otto
+            </a>
+            <a
+              className="contactLinks"
+              href="https://github.com/Bl4ckH4wkGER"
+              target={'_blank'}
+              rel="noreferrer">
+              <GitHubIcon />
+              Bl4ckH4wkGER
+            </a>
+          </Stack>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
